7654be497450ea921f8ca2884435a85b6ce67ba5
Daniel Freitas <daniel.italofreitas@gmail.com>
2020-08-13 21:41:22 -0300

    style: versao final de adaptação de estilos para iOS

--- App - Despesas Pessoais/expenses/lib/components/adaptative_text_field.dart
+++ App - Despesas Pessoais/expenses/lib/components/adaptative_text_field.dart
@@ -8,32 +8,37 @@ class AdaptativeTextField extends StatelessWidget {
   final TextInputType keyboardType;
   final Function(String) onSubmitted;
 
-  AdaptativeTextField({
-    this.label,
-    this.controller,
-    this.keyboardType = TextInputType.text,
-    this.onSubmitted
-  })
+  AdaptativeTextField(
+      {this.label,
+      this.controller,
+      this.keyboardType = TextInputType.text,
+      this.onSubmitted});
 
   @override
   Widget build(BuildContext context) {
-    return Platform.isIOS ? 
-    Padding(
-      padding: const EdgeInsets.only(bottom: 10,),
-      child: CupertinoTextField(
-        controller: controller,
-                        keyboardType: keyboardType,
-                  onSubmitted: onSubmitted,
-                  placeholder: label,
-                  padding: EdgeInsets.symmetric(horizontal: 6, vertical: 12,),
-      ),
-    )
-     : TextField( controller: controller,
-                keyboardType: keyboardType,
-                onSubmitted: onSubmitted,
-                decoration: InputDecoration(
-                  labelText: label,
-                ),
-                );
+    return Platform.isIOS
+        ? Padding(
+            padding: const EdgeInsets.only(
+              bottom: 10,
+            ),
+            child: CupertinoTextField(
+              controller: controller,
+              keyboardType: keyboardType,
+              onSubmitted: onSubmitted,
+              placeholder: label,
+              padding: EdgeInsets.symmetric(
+                horizontal: 6,
+                vertical: 12,
+              ),
+            ),
+          )
+        : TextField(
+            controller: controller,
+            keyboardType: keyboardType,
+            onSubmitted: onSubmitted,
+            decoration: InputDecoration(
+              labelText: label,
+            ),
+          );
   }
 }
